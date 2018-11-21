67adc1c8e24cb461125860ed2dee2da0ec43003c
Allsummerking <38412959+Allsummerking@users.noreply.github.com>
2018-11-21 15:29:13 +0800

    Update Part1.example.sh

--- Example/Part1.example.sh
+++ Example/Part1.example.sh
@@ -1,8 +1,8 @@
 ./s1.Find_PotentialSSR  --flank 150 --ssr2 6 --ssr3 4 --ssr4 4 --ssr5 4 --ssr6 4 short.fa ssr.out
 ./s2.Designer_Primer ssr.out raw_primer_file primer_results_file 
 ./s3.Primer_Filter primer_results_file rescreen_file blastin_file
-formatdb  -i short.fa -p F -o T
-blastall -i blastin_file  -d short.fa -p blastn -o prefix.blast.out -F F -b 10000 -v 10000
+./formatdb  -i short.fa -p F -o T
+./blastall -i blastin_file  -d short.fa -p blastn -o prefix.blast.out -F F -b 10000 -v 10000
  ./s5.Blast_Parse prefix.blast.out query_sbjct.out  stac.out  1
  ./s6.Primer_Pair_Generater query_sbjct.out primer.tab
 ./s7.Intermediate_Primer_Finder primer.tab rescreen_file  inter_primer.out 2000
